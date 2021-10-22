b3fd357e70b3911c88832e12b4971c4dd04d100f
Jacob <vb2553yq@go.minnstate.edu>
2021-10-21 21:31:14 -0500

    First push of the app to the repo

---
+++ android/app/src/main/kotlin/com/example/group2a3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.group2a3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import '/widgets/Home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Group 2 Github App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Group 2 Github App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+           mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Home()
+          ],
+        ),
+      ),
+    );
+  }
+}
